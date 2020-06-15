8a81c8a0583a8ed7448d38ef6ccbed7aaeca703f
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-06-15 19:43:50 -0300

    Agregue primeras validaciones e imagenes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ const btn = document.getElementById('enviar');
 const nombre = document.getElementById('nombre');
 const apellido = document.getElementById('apellido');
 
-const respuesta1 = document.getElementById('respuesta1');
-const resultado1 = document.getElementById('resultado1');
+let total = 0;
+
+const preg1 = document.getElementById('preg1');
+const rta1 = document.getElementById('rta1');
+const preg2 = document.getElementById('preg2');
+const rta2 = document.getElementById('rta2');
+
 
 btn.addEventListener('click', (ev) => {
     ev.preventDefault();
@@ -11,7 +16,7 @@ btn.addEventListener('click', (ev) => {
     // Validaciones
 
     // Nombre
-    if(nombre.value.length < 3){
+    if (nombre.value.length < 3) {
         nombre.style.border = "2px solid red";
     }
     else {
@@ -19,12 +24,33 @@ btn.addEventListener('click', (ev) => {
     }
 
     // Apellido
-    if(apellido.value.length < 3){
+    if (apellido.value.length < 3) {
         apellido.style.border = "2px solid red";
     }
     else {
         apellido.style.border = "2px solid green";
     }
+
+    // Pregunta 1
+
+    if (rta1.checked) {
+        total += 10
+        preg1.style.color = "green";
+
+    } else {
+        preg1.style.color = "red";
+    }
+
+    // Pregunta 2
+
+    if (rta2.checked) {
+        total += 10
+        preg2.style.color = "green";
+
+    } else {
+        preg2.style.color = "red";
+    }
+
 
 
 });
